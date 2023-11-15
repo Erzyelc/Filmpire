8339c33e13588c2b81892829354c18ca18c259b7
Ryan Carpenter <scrian_12@yahoo.com>
2023-11-15 16:31:52 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { CssBaseline } from "@mui/material";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello, World - Filmpire</h1>
+    </div>
+  );
+};
+
+export default App;
