785d49eee3bd1d5556b752e95818589ea7eb81bc
Ryan Carpenter <scrian_12@yahoo.com>
2023-11-20 16:23:07 -0500

    mistakenly forgot to update branch

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { ClassNames } from "@emotion/react";
+
+import useStyles from "./styles";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}></Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default NavBar;
