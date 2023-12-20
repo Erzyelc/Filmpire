5cda272797569b4e6b05041c7925ee145acba6cb
Ryan Carpenter <scrian_12@yahoo.com>
2023-12-20 16:54:15 -0500

    added actors page

--- src/components/Actors/styles.js
+++ src/components/Actors/styles.js
@@ -1,29 +1,10 @@
 import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme) => ({
-  containerSpaceAround: {
-    display: "flex",
-    justifyContent: "space-between",
-    margin: "10px 0 !important",
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      flexWrap: "wrap",
-    },
-  },
-  poster: {
+  image: {
+    maxWidth: "80%",
     borderRadius: "20px",
-    boxShadow: ".5em 1em 1em rgb(64, 64, 70)",
-    width: "70%",
-    [theme.breakpoints.down("md")]: {
-      margin: "0 auto",
-      width: "50%",
-      height: "28rem",
-    },
-    [theme.breakpoints.down("sm")]: {
-      margin: "0 auto",
-      width: "100%",
-      height: "350px",
-      marginBottom: "30px",
-    },
+    objectFit: "cover",
+    boxShadow: "0.5em 0.5em 1em",
   },
 }));
