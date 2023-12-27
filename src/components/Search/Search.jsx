c1b1d80c20c139178900cffcca42f98a0b23038a
Ryan Carpenter <scrian_12@yahoo.com>
2023-12-27 16:55:40 -0500

    added responsive fixes

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -11,6 +11,7 @@ const Search = () => {
   const classes = useStyles();
   const [query, setQuery] = useState("");
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
@@ -18,6 +19,10 @@ const Search = () => {
       dispatch(searchMovie(query));
     }
   };
+
+  if (location.pathname !== "/search") {
+    return null;
+  }
 
   return (
     <div className={classes.searchContainer}>
